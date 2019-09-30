8a3d04198be142798943e597fa51f80f0b390fff
denis <dineresc@gmail.com>
2019-09-30 20:42:27 -0300

    connecting front with server

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')();
 
-express.use((req, res, next) => {
-    console.log("first middleware");
+express.use((req,res,next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, DELETE, PUT');
     next();
+
 });
 
-express.use((req, res, next) => {
-    console.log("second middleware");
+express.use('/api/notes',(req, res, next) => {
+
+     posts = [
+        { id: 'idnumber1',title: 'First note', content: 'this was created auto by code'},
+        { id: 'idnumber2',title: 'Sec note', content: 'this was created auto by code'},
+        { id: 'idnumber3',title: 'Third note', content: 'this was created auto by code'},
+        { id: 'idnumber4',title: 'Fourth note', content: 'this was created auto by code'}
+
+    ];
+
+    res.status(200).json({
+        message: 'Posts fetched',
+        posts: posts
+    });
 });
 
 module.exports = express;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  
+  constructor(private _http: HttpClient){}
+
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  getPosts() {
-    return [...this.posts];
+  getPosts() {  
+    this._http.get<{message:string, posts:Post[]}>('http://localhost:3000/api/notes').subscribe((resData) => {
+      this.posts = resData.posts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   getPostUpdateListener() {
@@ -17,7 +24,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = {id:null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
