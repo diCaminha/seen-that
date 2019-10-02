5415e00054d96135577d35c5a854aa3ba4ec910c
denis <dineresc@gmail.com>
2019-10-02 18:17:13 -0300

    adding content as link to website

---
+++ src/app/posts/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { PostListComponent } from "./post-list/post-list.component";
+import { PostCreateComponent } from "./post-create/post-create.component";
+
+const routes: Routes = [
+    {path: '', component: PostListComponent},
+    {path: 'create', component: PostCreateComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
